dfdc1cd0f42516a17e9fd4d0fe74bbd23dba863f
SahilJangid2611 <105125368+SahilJangid2611@users.noreply.github.com>
2025-01-31 22:07:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function removeColor() {
 				// Get the select element
 				const colorSelect = document.getElementById('colorSelect');
 				
-				// Get the selected option
+				// Get the selected option value
 				const selectedColor = colorSelect.value;
-				
+
 				// Loop through the options and remove the selected one
 				for (let i = 0; i < colorSelect.options.length; i++) {
 					if (colorSelect.options[i].value === selectedColor) {
-						colorSelect.remove(i);
+						colorSelect.options[i].remove(); // Remove the option directly
 						break; // Exit the loop once the item is removed
 					}
 				}
