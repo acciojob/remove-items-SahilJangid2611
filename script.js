dd7c1b174921fc090045d4fd819699e5bf81de7f
SahilJangid2611 <105125368+SahilJangid2611@users.noreply.github.com>
2025-01-31 22:13:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function removeColors() {
 				// Get the select element
 				const colorSelect = document.getElementById('colorSelect');
 				
+				// Get all selected options
+				const selectedOptions = Array.from(colorSelect.selectedOptions); // Convert selectedOptions to an array
+
 				// Loop through the selected options and remove them
-				for (let i = colorSelect.options.length - 1; i >= 0; i--) {  // Loop backwards to avoid skipping options
-					if (colorSelect.options[i].selected) {
-						colorSelect.options[i].remove();
-					}
-				}
+				selectedOptions.forEach(option => {
+					option.remove();
+				});
 			}
